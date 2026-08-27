8095ceb94f8102ffbc3bab2e5b21003dc7a20780
cosorioc <carlos.osorioc@sabbagradiologos.com>
2026-08-27 17:25:15 -0500

    api routes in advance, likes and feed complete

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -8,21 +8,21 @@ configDotenv({
 import { initializeApp, getApps, cert } from "firebase-admin/app";
 
 import { getAuth } from "firebase-admin/auth";
-import { getFirestore } from "firebase-admin/firestore";
+import { getFirestore, FieldValue } from "firebase-admin/firestore";
 import { getDatabase } from "firebase-admin/database";
 import { getStorage } from "firebase-admin/storage";
 
 const firebaseApp = getApps().length
   ? getApps()[0]
   : initializeApp({
-      credential: cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-      }),
-      databaseURL: process.env.FIREBASE_DB_URL,
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    });
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
+    databaseURL: process.env.FIREBASE_DB_URL,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  });
 
 const auth = getAuth(firebaseApp);
 const db = getFirestore(firebaseApp);
@@ -32,4 +32,4 @@ const bucket = getStorage(firebaseApp).bucket();
 console.log("Firebase conectado correctamente");
 console.log("Storage:", bucket.name);
 
-export { firebaseApp, auth, db, realtimeDb, bucket };
+export { firebaseApp, auth, db, realtimeDb, bucket, FieldValue };
